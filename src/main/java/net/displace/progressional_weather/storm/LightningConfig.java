21a907e203a1ccf7b6edda9bd5038204fc668afe
elicl <eliclay1@byu.edu>
2026-01-01 17:22:31 -0500

    Created LightningConfig to breakdown json arguments.

---
+++ src/main/java/net/displace/progressional_weather/storm/LightningConfig.java
@@ -0,0 +1,14 @@
+package net.displace.progressional_weather.storm;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
+public record LightningConfig(float strikeChance, float damage) {
+    public static final Codec<LightningConfig> CODEC = RecordCodecBuilder.create(
+            instance ->
+            instance.group(
+                    Codec.FLOAT.fieldOf("strike_chance").forGetter(LightningConfig::strikeChance),
+                    Codec.FLOAT.fieldOf("damage").forGetter(LightningConfig::damage)
+            ).apply(instance, LightningConfig::new)
+            );
+}
