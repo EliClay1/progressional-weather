d7cdb1a0ca21cefb19491fd0908565254c8620a9
elicl <eliclay1@byu.edu>
2026-01-01 16:00:19 -0500

    Implemented CODEC for StormType enum

--- src/main/java/net/displace/progressional_weather/storm/enums/StormType.java
+++ src/main/java/net/displace/progressional_weather/storm/enums/StormType.java
@@ -1,7 +1,23 @@
 package net.displace.progressional_weather.storm.enums;
 
-public enum StormType {
-    RAIN,
-    THUNDERSTORM
+import com.mojang.serialization.Codec;
+import net.minecraft.util.StringRepresentable;
+import org.jspecify.annotations.NonNull;
+
+public enum StormType implements StringRepresentable {
+    // Add more storm types here.
+    THUNDERSTORM("thunderstorm");
+
+    private final String name;
+    StormType(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public @NonNull String getSerializedName() {
+        return name;
+    }
+
+    public static final Codec<StormType> CODEC = StringRepresentable.fromEnum(StormType::values);
 }
 
