cf976ba14052970b51613a8f418104db2fc4c944
elicl <eliclay1@byu.edu>
2026-01-01 17:41:14 -0500

    Shifted storm codec to StormData

--- src/main/java/net/displace/progressional_weather/storm/StormData.java
+++ src/main/java/net/displace/progressional_weather/storm/StormData.java
@@ -1,6 +1,8 @@
 package net.displace.progressional_weather.storm;
 
+import com.mojang.serialization.Codec;
 import com.mojang.serialization.Dynamic;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.displace.progressional_weather.storm.enums.StormDirection;
 import net.displace.progressional_weather.storm.enums.StormType;
 
@@ -30,6 +32,26 @@ public record StormData(
             throw new IllegalArgumentException("min_duration & max_duration cannot be less than 0!");
         }
     }
+
+    // defines the min and max validation range for storm tiers.
+    private static final Codec<Integer> TIER_CODEC = Codec.intRange(1, 3);
+
+    public static Codec<StormData> STORM_DATA_CODEC = RecordCodecBuilder.create(instance ->
+            instance.group(
+                    Codec.STRING.fieldOf("id").forGetter(StormData::id),
+                    StormType.CODEC.fieldOf("type").forGetter(StormData::type),
+                    TIER_CODEC.fieldOf("tier").orElse(1).forGetter(StormData::tier),
+                    StormDirection.CODEC.fieldOf("direction").forGetter(StormData::direction),
+                    Codec.FLOAT.fieldOf("wind_strength").forGetter(StormData::windStrength),
+                    Codec.INT.fieldOf("min_duration").forGetter(StormData::minDuration),
+                    Codec.INT.fieldOf("max_duration").forGetter(StormData::maxDuration),
+                    LightningConfig.CODEC.optionalFieldOf("lightning").forGetter(StormData::lightning),
+                    Codec.BOOL.fieldOf("can_escalate").forGetter(StormData::canEscalate),
+                    Codec.BOOL.fieldOf("naturally_generates").forGetter(StormData::naturallyGenerates),
+                    Codec.unboundedMap(Codec.STRING, Codec.PASSTHROUGH)
+                            .optionalFieldOf("properties", Map.of()).forGetter(StormData::properties)
+            ).apply(instance, StormData::new)
+    );
 
 }
 
