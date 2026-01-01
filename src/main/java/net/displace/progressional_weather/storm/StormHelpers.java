5fee37a71a2a43817e6659dd031babdfa2fc1f16
elicl <eliclay1@byu.edu>
2026-01-01 17:23:16 -0500

    Added a base class StormHelpers that will hold helper functions used across the storm management system.

---
+++ src/main/java/net/displace/progressional_weather/storm/StormHelpers.java
@@ -0,0 +1,21 @@
+package net.displace.progressional_weather.storm;
+
+public class StormHelpers {
+
+/*
+* var multiplier = stormData.properties().get("mob_spawn_multiplier");
+// multiplier is a Dynamic<?>, not the original value
+
+// To use it:
+if (multiplier instanceof Dynamic<?> dynamic) {
+    float value = dynamic.asNumber()
+        .result()
+        .orElse(1.0f)
+        .floatValue();
+}
+*
+*
+*
+* */
+
+}
